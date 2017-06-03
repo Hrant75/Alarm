42a94542a098d2c59eeae4b2da39a46cb055839a
Hrant Hayrapetyan <hhro@yahoo.com>
2017-06-03 04:02:26 +0400

    Alarm

--- app/src/main/java/com/example/hrant/alarmapp/AlarmReceiverNotification.java
+++ app/src/main/java/com/example/hrant/alarmapp/AlarmReceiverNotification.java
@@ -4,6 +4,8 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 
@@ -16,16 +18,15 @@ public class AlarmReceiverNotification extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.mipmap.ic_launcher_round)
-                        .setContentTitle("Alarm notification")
-                        .setContentText("Alarm Alarm Alarm !!!");
+                    .setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setContentTitle("Alarm notification")
+                    .setContentText("Alarm Alarm Alarm !!!")
+                    .setVibrate(new long[]{200, 200})
+                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM))
+                    .setLights(0xFF0000FF, 1000, 1000);
 
-        int mNotificationId = 001;
         NotificationManager mNotifyMgr =
                 (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
-        mNotifyMgr.notify(mNotificationId, mBuilder.build());
-
-        Vibrator vibrator = (Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE);
-        vibrator.vibrate(200);
+        mNotifyMgr.notify(1, mBuilder.build());
     }
 }
